38b4a219bd35f24b8ddd7398605ee23b5e5d8919
KSS095 <tmdtn339@gmail.com>
2025-08-14 01:27:33 +0900

    add .gitignore

---
+++ SWEA/1952_수영장/수영장_승수.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('sample_input.txt')
+
+def swimming_pool(idx, cost):
+    global least_cost
+
+    if idx >= 12:   # 1년 이용 계획을 짰다면
+        least_cost = min(least_cost, cost)   # 최소비용 갱신
+        return
+    if cost > least_cost: return    # 가지치기
+
+    swimming_pool(idx + 1, cost + prices[0] * plan[idx])    # 1일 이용권
+    swimming_pool(idx + 1, cost + prices[1])    # 1개월 이용권
+    swimming_pool(idx + 3, cost + prices[2])    # 3개월 이용권
+    swimming_pool(12, prices[3])    # 1년 이용권
+
+
+T = int(input())
+for tc in range(1, T + 1):
+    prices = list(map(int, input().split()))
+    plan = list(map(int, input().split()))
+    least_cost = 3000 * 30 * 31  # 나올 수 있는 최대 요금
+
+    swimming_pool(0, 0)     # idx = 0 부터 시작, cost = 0 부터 시작
+
+    print(f'#{tc} {least_cost}')
