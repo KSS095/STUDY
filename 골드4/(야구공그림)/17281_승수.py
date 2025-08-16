27f019aa99a2d3a3fafd085fbee2b320026e27a7
KSS095 <tmdtn339@gmail.com>
2025-08-16 22:23:47 +0900

    2025_08_16 at home

--- 골드4/(야구공그림)/17281_승수.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.stdin = open('input.txt')
-
-for _ in range(6):
-    inning = int(input())
-    inning_result = [list(map(int, input().split())) for _ in range(inning)]
-    print(inning_result)
-
-    # print(result)
