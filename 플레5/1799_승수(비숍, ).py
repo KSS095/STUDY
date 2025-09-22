6fbb81be7254fa5f80b2b7dca08036e042fce295
KSS095 <tmdtn339@gmail.com>
2025-09-22 21:56:59 +0900

    2025_09_22

--- 플레5/1799_승수(비숍, ).py
+++ 플레5/1799_승수(비숍, ).py
@@ -1,30 +1,97 @@
-# 따라서 이 정의를 따르면 체스판을 색칠하는 경우는 두 가지뿐이다.
-# 하나는 맨 왼쪽 위 칸이 흰색인 경우, 하나는 검은색인 경우이다.
+# N = int(input())
+# board = [list(map(int, input().split())) for _ in range(N)]
 
-N, M = map(int, input().strip().split())
-board = [input().strip() for _ in range(N)]
+# diag1 = [False] * (2*N)   # 우하향 대각선
+# diag2 = [False] * (2*N)   # 좌하향 대각선
 
-# 최대로 다시 칠해야 하는 횟수는 64번
-repaint = 64
+# pos_black, pos_white = [], []
+# for i in range(N):
+#     for j in range(N):
+#         if board[i][j] == 1:   # 비숍 놓을 수 있는 칸
+#             if (i+j) % 2 == 0:
+#                 pos_black.append((i, j))
+#             else:
+#                 pos_white.append((i, j))
 
-# 8 X 8 크기의 체스판으로 자른 모든 경우의 수 구하기
-for x in range(N - 7):
-    for y in range(M - 7):
-        # 다시 칠해야 하는 횟수
-        cnt = 0
+# def dfs(idx, cnt, positions):
+#     if idx == len(positions):
+#         return cnt
 
-        # 맨 왼쪽 위 칸이 흰색인 경우와 비교
-        for i in range(8):
-            for j in range(8):
-                if (i + j) % 2 == 0:
-                    expected = 'W'
-                else:
-                    expected = 'B'
+#     x, y = positions[idx]
+#     res = dfs(idx+1, cnt, positions)  # 놓지 않음
+#     if not diag1[x+y] and not diag2[x-y+N]:
+#         diag1[x+y] = diag2[x-y+N] = True
+#         res = max(res, dfs(idx+1, cnt+1, positions))
+#         diag1[x+y] = diag2[x-y+N] = False
+#     return res
 
-                # expected와 다르다면 다시 칠해야 하므로 cnt++
-                if board[x + i][y + j] != expected:
-                    cnt += 1
-        # 64 - cnt는 맨 왼쪽 위 칸이 검은색인 경우에서의 cnt
-        repaint = min(repaint, cnt, 64 - cnt)
+# ans = dfs(0, 0, pos_black) + dfs(0, 0, pos_white)
+# print(ans)
 
-print(repaint)
+
+def is_valid_pos(board, row, col):
+    # 좌상향 대각선 위 체크
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # 우상향 대각선 위 체크
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def dfs(idx, positions, board, cnt):
+    # 모든 칸을 다 확인했다면 최대 개수 갱신
+    global max_cnt
+    if idx == len(positions):
+        max_cnt = max(max_cnt, cnt)
+        return
+
+    x, y = positions[idx]
+
+    # 현재 칸에 놓는 경우
+    if is_valid_pos(board, x, y):
+        board[x][y] = 1
+        dfs(idx + 1, positions, board, cnt + 1)
+        board[x][y] = 0
+
+    # 현재 칸에 놓지 않는 경우
+    dfs(idx + 1, positions, board, cnt)
+
+
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(n)]
+
+# 흑/백 칸 분리
+
+# 체스판을
+# 흑 백 흑 백
+# 백 흑 백 흑
+# 흑 백 흑 백
+# 백 흑 백 흑
+# 으로 생각하면 (행+열)의 합이 짝수인 칸은 흑, 홀수인 칸은 백
+# 서로 공격할 수 없으므로 흑/백 칸을 나누어 각각 독립적으로 탐색 가능 -> 시간 단축
+
+black_positions = []
+white_positions = []
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:  # 비숍 놓을 수 있는 칸
+            if (i + j) % 2 == 0:
+                black_positions.append((i, j))
+            else:
+                white_positions.append((i, j))
+
+# 흑/백 독립 탐색
+max_cnt = 0
+dfs(0, black_positions, [[0]*n for _ in range(n)], 0)
+black_max = max_cnt
+
+max_cnt = 0
+dfs(0, white_positions, [[0]*n for _ in range(n)], 0)
+white_max = max_cnt
+
+print(black_max + white_max)
