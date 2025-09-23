b3514898b891425fcd40e69951bfbe6302bb4808
KSS095 <tmdtn339@gmail.com>
2025-09-23 17:57:54 +0900

    2025_09_23

---
+++ 골드4/5427_승수(불).py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for tc in range(T):
+    w, h = map(int, input().split())
+
+    building = [list(input().split()) for _ in range(h)]
+    stars_index = []
+
+    for i in range(h):
+        for j in range(w):
+            if building[i][j] == '*':
+                stars_index.append((i, j))
